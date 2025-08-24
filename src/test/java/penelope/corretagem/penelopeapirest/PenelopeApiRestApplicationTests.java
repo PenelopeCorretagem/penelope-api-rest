a80b243323d255ce6934aa91a21d907519297b9c
kenner.lima <kenner.lima@sinqia.com.br>
2025-08-23 22:39:19 -0300

    Configuração de ambiente: swagger, dependências, estrutura de pacotes, configurações de ambiente, .gitignore e endpoint de teste

---
+++ src/test/java/penelope/corretagem/penelopeapirest/PenelopeApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package penelope.corretagem.penelopeapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenelopeApiRestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
