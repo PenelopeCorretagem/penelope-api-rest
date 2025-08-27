4941ddabb41c6923be3c597cda1a351d7afea6c9
RennanSMoura <rennan.moura@sptech.school>
2025-08-27 20:20:21 -0300

    Ajuste entidade

--- src/main/java/penelope/corretagem/penelopeapirest/entity/UserEntity.java
+++ src/main/java/penelope/corretagem/penelopeapirest/entity/UserEntity.java
@@ -5,13 +5,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class UserEntity {
 
     @Id
@@ -22,52 +26,4 @@ public class UserEntity {
     private String email;
     private Date dtNascimento;
     private Double rendaMensal;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Date getDtNascimento() {
-        return dtNascimento;
-    }
-
-    public void setDtNascimento(Date dtNascimento) {
-        this.dtNascimento = dtNascimento;
-    }
-
-    public Double getRendaMensal() {
-        return rendaMensal;
-    }
-
-    public void setRendaMensal(Double rendaMensal) {
-        this.rendaMensal = rendaMensal;
-    }
 }
