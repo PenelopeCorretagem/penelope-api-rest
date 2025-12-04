eb5d2535fb7ad3d8bbe5fc8e6c9a2152f8d55ec8
NicolasNun <nicolassantos9283@gmail.com>
2025-12-03 23:46:35 -0300

    feat: adicionando endpoint para retornar todos os amenities

---
+++ src/main/java/penelope/corretagem/penelopeapirest/controller/AmenitiesController.java
@@ -0,0 +1,36 @@
+package penelope.corretagem.penelopeapirest.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import penelope.corretagem.penelopeapirest.data.domain.dto.AdvertisementDTO.AmenitiesResponse;
+import penelope.corretagem.penelopeapirest.service.AmenitiesService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/amenities")
+public class AmenitiesController {
+
+    private final AmenitiesService amenitiesService;
+
+    public AmenitiesController(AmenitiesService amenitiesService) {
+        this.amenitiesService = amenitiesService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AmenitiesResponse>> getAmenities() {
+        try {
+            List<AmenitiesResponse> amenities = amenitiesService.getAllAmenities();
+
+            if (amenities.isEmpty()) {
+                return ResponseEntity.noContent().build();
+            }
+
+            return ResponseEntity.ok(amenities);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao buscar diferencias", e);
+        }
+    }
+}
