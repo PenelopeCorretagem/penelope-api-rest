09f9b3bd101d5c473d51e4fb21a52f41f09d8cd3
RennanSMoura <rennan.moura@sptech.school>
2026-04-06 18:04:05 -0300

    fix: ajustes de ambientes e correcao de filtros

--- src/main/java/penelope/corretagem/penelopeapirest/infrastructure/config/SecurityConfig.java
+++ src/main/java/penelope/corretagem/penelopeapirest/infrastructure/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package penelope.corretagem.penelopeapirest.infrastructure.config;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,9 +28,11 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 public class SecurityConfig {
 
     private final SecurityFilter securityFilter;
+    private final Environment environment;
 
-    public SecurityConfig(SecurityFilter securityFilter) {
+    public SecurityConfig( SecurityFilter securityFilter, Environment environment) {
         this.securityFilter = securityFilter;
+        this.environment = environment;
     }
 
     private static final String[] AUTH_WHITELIST = {
@@ -36,7 +41,6 @@ public class SecurityConfig {
             "/contact-us",
             "/swagger-ui/**",
             "/api-docs/**",
-            "/h2-console/**",
 
             "/users/forgot-password",
             "/users/reset-password",
@@ -46,7 +50,6 @@ public class SecurityConfig {
 
             "/advertisements",
             "/advertisements/{id}"
-
     };
 
     @Bean
@@ -56,21 +59,24 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .headers(headers -> headers.frameOptions(frame -> frame.disable()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(PathRequest.toH2Console()).permitAll()
-                        .requestMatchers(AUTH_WHITELIST).permitAll()
-                        // Permite criar usuário sem estar logado
-                        .requestMatchers(antMatcher(HttpMethod.POST, "/users")).permitAll()
-                        // Permite ver anúncios sem estar logado
-                        .requestMatchers(antMatcher(HttpMethod.GET, "/advertisements/**")).permitAll()
+                .authorizeHttpRequests(authorize -> {
 
-                        .anyRequest().authenticated()
-                )
+                    if (environment.acceptsProfiles(Profiles.of("dev"))) {
+                        authorize.requestMatchers(PathRequest.toH2Console()).permitAll();
+                    }
+
+                    authorize
+                            .requestMatchers(AUTH_WHITELIST).permitAll()
+                            // Permite criar usuário sem estar logado
+                            .requestMatchers(antMatcher(HttpMethod.POST, "/users")).permitAll()
+                            // Permite ver anúncios sem estar logado
+                            .requestMatchers(antMatcher(HttpMethod.GET, "/advertisements/**")).permitAll()
+                            .anyRequest().authenticated();
+                })
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
-    // Define as configurações de CORS para permitir requisições do frontend.
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
@@ -81,9 +87,7 @@ public class SecurityConfig {
                 "http://localhost:3002"   // prod
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        // Cabeçalhos permitidos
         configuration.setAllowedHeaders(List.of("*"));
-        // Permite credenciais
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
