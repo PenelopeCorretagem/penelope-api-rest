969397ffb8ed2a56ddfd5b49c9a29169124d56ca
RennanSMoura <rennan.moura@sptech.school>
2026-04-26 13:12:22 -0300

    fix: ajuste de segurança para cliente visualizar seu proprio perfil e adm visualizar todos

--- src/main/java/penelope/corretagem/penelopeapirest/application/useCase/user/GetUserByIdUseCase.java
+++ src/main/java/penelope/corretagem/penelopeapirest/application/useCase/user/GetUserByIdUseCase.java
@@ -1,10 +1,12 @@
 package penelope.corretagem.penelopeapirest.application.useCase.user;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import penelope.corretagem.penelopeapirest.application.dto.UserResponse;
 import penelope.corretagem.penelopeapirest.core.exception.ResourceNotFoundException;
 import penelope.corretagem.penelopeapirest.core.user.repository.IUserRepository;
-import penelope.corretagem.penelopeapirest.application.dto.UserResponse;
+import penelope.corretagem.penelopeapirest.core.user.valueObject.AccessLevel;
 
-public class    GetUserByIdUseCase {
+public class GetUserByIdUseCase {
 
     private final IUserRepository userRepository;
 
@@ -13,8 +15,19 @@ public class    GetUserByIdUseCase {
     }
 
     public UserResponse execute(Long id) {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var currentUserEmail = authentication.getName();
+
         var user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
+
+        boolean isAdministrador = authentication.getAuthorities().stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority()
+                        .equals("ROLE_" + AccessLevel.ADMINISTRADOR.name()));
+
+        if (!isAdministrador && !user.getEmail().equals(currentUserEmail)) {
+            throw new ResourceNotFoundException("Usuário não encontrado");
+        }
 
         return new UserResponse(
                 user.getId(), user.getName(), user.getEmail(),
